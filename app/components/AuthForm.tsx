6b210c28d3c6a81b0c8ca4ca4ef3a4a6c2191e58
Manoranjan1k <manasj790@gmail.com>
2025-07-09 22:42:51 +0530

    Refactor AuthForm component to accept 'type' prop and update form structure

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 import {
   Form,
   FormControl,
@@ -21,7 +22,7 @@ const formSchema = z.object({
   username: z.string().min(2).max(50),
 })
 
-const AuthForm = () => {
+const AuthForm = ({type} : {type : FormType}) => {
    // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -36,7 +37,7 @@ const AuthForm = () => {
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
-
+  const isSignIn = type === "sign-in"
   return (
     <div className="card-border lg:min-w-[566px]">
       <div className=" flex flex-col gap-6 card py-14 px-10">
@@ -44,29 +45,23 @@ const AuthForm = () => {
           <Image src="/logo.svg" alt="logo" height={32} width={38}/>
           <h2 className="text-primary-100">Prepwise</h2>
           </div>
-      </div> 
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+          <h3>Practice job interview with AI</h3>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+            {!isSignIn && <p>Name</p>}
+            <p>Email</p>
+            <p>Password</p>
+            <Button type="submit">{isSignIn ? 'Sign in' : 'create an Account'}</Button>
+           </form>
+        </Form>
+        <p className="text-center">
+          {isSignIn ? "Don't have an account?" : "Already have an account?"}
+          <Link href={isSignIn ? "/sign-up" : "/sign-in"} className="font-bold text-user-primary ml-1">
+            {isSignIn ? "Sign up" : "Sign in"}
+          </Link>
+        </p>
     </div>
+   </div> 
   )
 }
 
